8df2c5257dd55d787d6b3af39b793cc9f811b8c4
Joe Cuenca <142828652+jcuenca93@users.noreply.github.com>
2024-03-19 17:25:39 +0000

    Tue, Mar 19, 2024, 10:25 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -5,10 +5,10 @@ canvas = document.getElementById('canvas')
 ctx = canvas.getContext('2d')
 
 //create ball properties
-ball ={
+ball = {
     x:canvas.width / 2,
     y:canvas.heaight /2
-    size:10,
+    size: 10,
     speed: 4,
     dx:4;
     dy:-4;
@@ -16,9 +16,12 @@ ball ={
 //drawball on canvas
 function drawBall(){
     ctx.beginPath()
-    ctx.arc(75,75,50,0,Math.PI*2,true)
+    ctx.arc(ball.x,ball.y,ball.size,0,Math.PI*2,true)
+    ctx.stroke()
 
 }
+drawBall
+
 
 
 
