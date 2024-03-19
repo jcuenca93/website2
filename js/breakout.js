4895b4d310f885d07811f889ff3aeb4950df9913
Joe Cuenca <142828652+jcuenca93@users.noreply.github.com>
2024-03-19 17:08:33 +0000

    Tue, Mar 19, 2024, 10:08 AM -07:00

---
+++ js/breakout.js
@@ -0,0 +1,11 @@
+rulesBtn - document.getElementById('rules-btn')
+rules = document.getElementById('rules')
+closeBtn = document.getElementById('close-btn')
+
+
+rulesBtn.addEventListener('click', () => {
+    rules.classList.add('show')
+})
+closeBtn.addEventListener('click',() => {
+    rules.classList.remove('show')
+})
