a846f6cceda419164bfba05d65ccfda4df3191ea
Joe Cuenca <142828652+jcuenca93@users.noreply.github.com>
2024-03-19 17:17:26 +0000

    Tue, Mar 19, 2024, 10:17 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -4,6 +4,26 @@ closeBtn = document.getElementById('close-btn')
 canvas = document.getElementById('canvas')
 ctx = canvas.getContext('2d')
 
+//create ball properties
+ball ={
+    x:canvas.width / 2,
+    y:canvas.heaight /2
+    size:10,
+    speed: 4,
+    dx:4;
+    dy:-4;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 rulesBtn.addEventListener('click', () => {
     rules.classList.add('show')
