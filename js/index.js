3f2d3cc60b7b926b32f8cbd6b6063f893a2a54af
Joe Cuenca <142828652+jcuenca93@users.noreply.github.com>
2024-05-24 05:48:03 +0000

    Thu, May 23, 2024, 10:48 PM -07:00

--- js/index.js
+++ js/index.js
@@ -2,15 +2,6 @@ btn = document.querySelector('.btn')
 button = document.getElementById('start-button')
 link = document.getElementById('links')
 
-btn.addEventListener('click', () => {
-    var randomNumber1 = Math.floor(Math.random() * 256);
-    var randomNumber2 = Math.floor(Math.random() * 256);
-    var randomNumber3 = Math.floor(Math.random() * 256);
-    var rgb = "rgb(" + randomNumber1 + "," + randomNumber2 + "," + randomNumber3 + ")";
-    document.body.style.backgroundColor = rgb;
-    document.body.classList.remove('instant-color-change');
-
-})
 
 button.addEventListener('click', () => {
 button.classList.add('invisible')
